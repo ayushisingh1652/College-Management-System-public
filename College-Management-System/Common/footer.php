d5028e1591b249c4ee49cea0585a56598ef77da0
Ayushi Singh <120139890+ayushisingh1652@users.noreply.github.com>
2023-01-06 00:22:21 +0530

    Add files via upload

---
+++ College-Management-System/Common/footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>ABC College</h3>
+            <p>This is a mini project made by Ayushi Singh.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About ABC</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">Civil Engineering</a></li>
+              <li><a href="">Electrical Engineering</a></li>
+              <li><a href="">Mechanical Engineering</a></li>
+              <li><a href="">BCA/MCA</a></li>
+              <li><a href="">Computer Science and engineering</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: ABC COLLEGE,
+                  , Jankipuram,Lucknow-226021 Uttar Pradesh – India</p>
+              <p>Telephones: (+91 9956220230)</p>
+              <p>E-mail: ayushis.domain@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text"></p>
+</footer>
